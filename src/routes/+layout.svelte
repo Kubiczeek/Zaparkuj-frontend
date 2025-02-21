82da6bbc426dea55e0137fa17e1a6673227b2019
Kubiczeek <jakubakubek@gmail.com>
2025-02-21 18:28:45 +0100

    feat(pwa): implement service worker registration and add PWA support with manifest

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,3 +1,13 @@
+<script>
+    import {onMount} from "svelte";
+
+	onMount(() => {
+		if ('serviceWorker' in navigator) {
+			navigator.serviceWorker.register('/sw.js');
+		}
+    })
+</script>
+
 <slot></slot>
 
 <style>
